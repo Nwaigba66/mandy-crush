10801875d329def75cb7e192e19b7d4e53758ef5
Nwaigba66 <glorianwaigba@gmail.com>
2024-03-01 14:11:10 +0100

    updated codebase

--- script/splash.js
+++ script/splash.js
@@ -1,24 +1,26 @@
 const splashScreen = document.getElementById('splash-screen')
 const startButton = document.getElementById('start-button');
 const restartBtn = document.getElementById('restart-button');
+const restartScreen = document.getElementById('game-end')
+const returnScreen = document.getElementById('game-end')
 const gameScreen = document.getElementById('game-screen')
 const gameContainer = document.getElementById('game-container')
 const returnBtn = document.getElementById("return-button")
 const finishMessage = document.getElementById('finish-game')
+const scoreDisplay = document.getElementById('score')
 const mainSong = new Audio("./audio/candy.mp4"); 
 const timer = document.getElementById('timer')
 
 startButton.addEventListener('click',  startGame);
-restartBtn.addEventListener('click', restartGame)
-returnBtn.addEventListener('click', startGame)
+restartBtn.addEventListener('click', startGame)
+
 
 let game = undefined
-let moves = 0;
-let startTime = 101;
+let startTime = 11;
 let timerId = null
 
 mainSong.loop = true;
-mainSong.volume = 0.3;
+mainSong.volume = 0.2;
 mainSong.play();
 
 function startGame() {
@@ -27,7 +29,8 @@ function startGame() {
   gameScreen.style.display = 'flex'
   gameContainer.style.display = 'block'
   moves = 0;
-  startTime = 101;
+  startTime = 11;
+  timer.innerText = `${startTime}s`
   timerId = setInterval(updateTimer, 1000);
 }
 
@@ -44,7 +47,7 @@ function gameOver() {
   clearInterval(timerId);
   startTime--;
   timer.innerText = `${startTime}s`
-  if (score.innerText >= 100 || score === 0) {
+  if (score.innerText >= 100) {
     finishMessage.innerText = 'Congratulations! You Win 🏅'
   } else if (moves >= 50 || score === 0 ) {
     finishMessage.innerText = 'Sorry! Try Again 🥺'
@@ -60,25 +63,34 @@ function end() {
   gameScreen.style.display = "flex"; 
   gameContainer.style.display = "block"
   finishMessage.style.display = 'block';
-  document.getElementById("finish-game").style.display = "none";
+  document.getElementById("finish-game").style.display = "block";
   
 }
 
 // Function to restart the game
 function restartGame() {
+  // window.location.reload()
   gameScreen.style.display = 'flex';
   splashScreen.style.display = 'none';
   restartScreen.style.display = 'none';
-  finishMessage.style.display = 'none';
-  document.getElementById("game-end").style.display = "none";
+  gameContainer.style.display = "block";
+  finishMessage.style.display = 'block';
+  restartBtn.style.display = 'none';
+  scoreDisplay.innerText = score;
+  startTime = 11;
+  score = 0;
+  moves = 0;
+  timer.innerText = `${startTime}s`
+  document.getElementById("restart-button").style.display = "none";
 }
 
-function returnGame() {
-  gameScreen.style.display = 'none';
-  splashScreen.style.display = 'block';
-  restartScreen.style.display = 'none';
-  document.getElementById("splash-screen").style.display = "block";
-}
+// function returnGame() {
+//   splashScreen.style.display = 'block';
+//   returnScreen.style.display = 'none';
+//   gameContainer.style.display = "none"
+//   finishMessage.style.display = 'none';
+//   document.getElementById("return-button").style.display = "block";
+// }
 
 
 
